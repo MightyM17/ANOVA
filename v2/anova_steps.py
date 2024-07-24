87410d80b083777d3232c500f06a4678d3d09263
MightyM17 <bavishimithil@gmail.com>
2024-07-24 20:31:34 +0530

    fd

--- v2/anova_steps.py
+++ v2/anova_steps.py
@@ -1,13 +1,8 @@
 import scipy.stats
 import pandas as pd
 
-# Function to preprocess data
-def preprocecss(data):
-    return data
-
 # CBD
 def anova_steps_cbd(data, a):
-    data = preprocecss(data)
     
     # Find mean of each group
     mean = data.mean()
@@ -61,7 +56,6 @@ def anova_steps_cbd(data, a):
     
 #RBD
 def anova_steps_rbd(data, a):
-    data = preprocecss(data)
     
     # Find mean of each group
     mean = data.mean()
@@ -133,3 +127,109 @@ def anova_steps_rbd(data, a):
     print("F Critical Block: ", F_critical_block)
 
     return mean, total_mean, SSC, df_c, SSR, df_r, SSE, df_e, SST, df_t, MSC, MSR, MSE, F_treat, F_block, F_critical_treat, F_critical_block
+
+#FD
+def anova_steps_fd(data, a, n, row_treat):
+    col_treat = data.columns.size
+    N = data.size
+    
+    df_r = row_treat - 1
+    print("df_r: ", df_r)
+    df_c = col_treat - 1
+    print("df_c: ", df_c)
+    df_i = df_r * df_c
+    print("df_i: ", df_i)
+    df_e = row_treat * col_treat * (n - 1)
+    print("df_e: ", df_e)
+    df_t = N - 1
+    print("df_t: ", df_t)
+    
+    # Grand Total
+    GT = data.sum().sum()
+    corr_fact = (GT ** 2) / N
+    
+    print("GT: ", GT)
+    print("corr_fact: ", corr_fact)
+    
+    # SST
+    SST = (data ** 2).sum().sum() - corr_fact
+    print("SST: ", SST)
+    
+    # SSC
+    column_sums = data.sum(axis=0)
+    SSC = (column_sums ** 2).sum() / (n*row_treat) - corr_fact
+    print("SSC: ", SSC)
+    
+    # SSR
+    total_rows = len(data)
+    block_sum = [0] * (total_rows // n)
+    i=0
+    for start_row in range(0, total_rows, n):
+        end_row = start_row + n
+        block_sum[i] = data.iloc[start_row:end_row].sum().sum()
+        i+=1
+    print(f"Sum of rows: {block_sum}")
+    SSR=0
+    for i in range(len(block_sum)):
+        SSR = SSR + (block_sum[i] ** 2)/(n*data.columns.size)
+    SSR = SSR - corr_fact
+    print("SSR: ", SSR)
+    
+    # SSI
+    block_sums = {col: [] for col in data.columns}  
+    for col in data.columns:
+        for start_row in range(0, len(data), n):
+            end_row = start_row + n
+            block_sum = data[col][start_row:end_row].sum()
+            block_sums[col].append(block_sum)
+    
+    print("Block sums: ", block_sums)
+
+    SSI = 0
+    for col, sums in block_sums.items():
+        for sum_val in sums:
+            SSI += (sum_val ** 2) / (n)
+    SSI = SSI - corr_fact - SSC - SSR
+    print("SSI: ", SSI)
+    
+    # SSE
+    SSE = SST - SSI - SSC - SSR
+    print("SSE: ", SSE)
+    
+    # Calculate MSR
+    MSR = SSR / df_r
+    print("MSR: ", MSR)
+    
+    # Calculate MSC
+    MSC = SSC / df_c
+    print("MSC: ", MSC)
+    
+    # Calculate MSI
+    MSI = SSI / df_i
+    print("MSI: ", MSI)
+    
+    # Calculate MSE
+    MSE = SSE / df_e
+    print("MSE: ", MSE)
+    
+    # Calculate F-Value
+    F_r = MSR / MSE
+    print("F_r: ", F_r)
+    F_c = MSC / MSE
+    print("F_c: ", F_c)
+    F_i = MSI / MSE
+    print("F_i: ", F_i)
+    
+    # Get F-Critical Value
+    F_critical_row = scipy.stats.f.ppf(1-a, df_r, df_e)
+    print("F Critical Treat: ", F_critical_row)
+    F_critical_col = scipy.stats.f.ppf(1-a, df_c, df_e)
+    print("F Critical Block: ", F_critical_col)
+    F_critical_iter = scipy.stats.f.ppf(1-a, df_i, df_e)
+    print("F Critical Iter: ", F_critical_iter)
+    
+    return SST, df_t, SSC, df_c, SSR, df_r, SSI, df_i, SSE, df_e, MSR, MSC, MSI, MSE, F_r, F_c, F_i, F_critical_row, F_critical_col, F_critical_iter
+
+# daaa = pd.DataFrame({'col1': [2,1,2,1,2,3,1,2], 'col2': [2,3,3,2,3,3,2,4], 'col3':[4,3,4,3,4,4,3,4]})
+# no_of_groups = 4
+# anova_steps_fd(daaa, 0.05, 4, 2)
